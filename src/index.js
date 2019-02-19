0a926c3637159fbab36d775a37f019aa8ad564bc
Anton <miklis2003@mail.ru>
2019-02-19 22:54:33 +0300

    first submit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  
+
+let length = preferences.length; 
+  let count = 0;  
+  for (let i = 0; i<length; i++) { 
+      let y = preferences[i] - 1;
+      if (y == i) continue;
+      let z = preferences[y] - 1;
+      if (y == z) continue;
+      let p = preferences[z] - 1;
+      if (p == z) continue;
+      if (p == i) {
+          count++;
+      }
+   }
+   return count / 3;
+
 };
